739497cdd21cdaa6fd744f9cc8222f4312920dc7
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-07-18 21:17:58 -0400

    updating repo

---
+++ utils/multi_func_threading.py
@@ -0,0 +1,6 @@
+"""
+This file will contain a function thats creates
+threads for several functions. It will be used
+for users who want to perform several functions
+simultaneously. Don't forget to use *args!
+"""
