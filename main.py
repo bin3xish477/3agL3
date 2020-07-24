8d7afe830b22e5f78e265a62ec111cb781dabeea
binexis <binexis@localhost.localdomain>
2020-07-24 14:47:14 -0400

    updating 3agL3

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-Objectives:
-	- sniff network packets and print out to stdout or to a file
-	- capture packets for a specified time and write packets to pcap
-	- provide the option for tcpdump filtering using the sniff(filter=) method
-	- open pcap files and parse data according to the 5 tuple
-"""
-
 from utils.parse_args import parse_args
 from datetime import datetime
 from src.net_sniff import NetSniff
