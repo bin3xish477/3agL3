bf08f7f10d431c1531e65ac824fe0efdf61c7d85
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-08-05 22:44:46 -0400

    updating 3agL3 repository

--- src/net_sniff.py
+++ src/net_sniff.py
@@ -4,7 +4,10 @@ Real-time network traffic capturing using scapy.all.sniff
 
 from datetime import datetime
 from colored import fg, attr
-from scapy.all import sniff, ICMP, IP, Ether
+from scapy.all import (
+	sniff, ICMP, IP, Ether, ARP,
+	DNS
+)
 
 class NetSniff:
 	def __init__(self, interf, berkeley_filter, count):
@@ -50,12 +53,61 @@ class NetSniff:
 					icmp_type = "echo-reply"
 				elif pkt[ICMP].type == 8:
 					icmp_type = "echo"
-
 				return (
 					f"<%s%s{date[11:13]}%s:%s%s{date[14:16]}%s:%s%s{date[17:25]}%s>" \
 					f" {src_mac} | {dst_mac} %s%s{str(pkt[IP].payload.name).upper()}%s" \
 					f" %s%s{pkt[IP].src}%s %s%s\u2192%s %s%s{pkt[IP].dst}%s" \
 					f" (TTL:{pkt[Ether].ttl} LEN:{pkt[Ether].len} TYPE:{icmp_type})"
+					% (
+						fg(141), attr("bold"), attr("reset"),
+						fg(141), attr("bold"), attr("reset"),
+						fg(141), attr("bold"), attr("reset"),
+						fg(118), attr("bold"), attr("reset"),
+						fg(208), attr("bold"), attr("reset"),
+						fg(9), attr("bold"), attr("reset"),
+						fg(220), attr("bold"), attr("reset"),
+					)
+				)
+			except:
+				return None
+		elif pkt.haslayer(ARP):
+			# Dont forget to extract ARP related information and print to stdout
+			try:
+				date = str(datetime.now())
+				src_mac = str(pkt[Ether].src)
+				dst_mac = str(pkt[Ether].dst)
+				proto = "ARP"
+				return (
+					f"<%s%s{date[11:13]}%s:%s%s{date[14:16]}%s:%s%s{date[17:25]}%s>" \
+					f" {src_mac} | {dst_mac}" \
+					f" %s%s{proto}%s" \
+					f" {pkt[IP].src}%s%s:{pkt[IP].sport}%s %s%s\u2192%s {pkt[IP].dst}%s%s:{pkt[IP].dport}%s" \
+					f"  (TTL:{pkt[0].ttl} LEN:{pkt[0].len})"
+					% (
+						fg(141), attr("bold"), attr("reset"),
+						fg(141), attr("bold"), attr("reset"),
+						fg(141), attr("bold"), attr("reset"),
+						fg(118), attr("bold"), attr("reset"),
+						fg(208), attr("bold"), attr("reset"),
+						fg(9), attr("bold"), attr("reset"),
+						fg(220), attr("bold"), attr("reset"),
+					)
+				)
+			except:
+				return None
+		elif pkt.haslayer(DNS):
+			# Dont forget to extract DNS related information and print to stdout
+			try:
+				date = str(datetime.now())
+				src_mac = str(pkt[Ether].src)
+				dst_mac = str(pkt[Ether].dst)
+				proto = "DNS"
+				return (
+					f"<%s%s{date[11:13]}%s:%s%s{date[14:16]}%s:%s%s{date[17:25]}%s>" \
+					f" {src_mac} | {dst_mac}" \
+					f" %s%s{proto}%s" \
+					f" {pkt[IP].src}%s%s:{pkt[IP].sport}%s %s%s\u2192%s {pkt[IP].dst}%s%s:{pkt[IP].dport}%s" \
+					f"  (TTL:{pkt[0].ttl} LEN:{pkt[0].len})"
 					% (
 						fg(141), attr("bold"), attr("reset"),
 						fg(141), attr("bold"), attr("reset"),
