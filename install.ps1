e5ad109facbbdc125672d84c2750549933d3ba2b
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-08-09 20:25:10 -0400

    updating 3agL3

--- install.ps1
+++ install.ps1
@@ -1,1 +1,7 @@
+Write-Output "Cloning 3agL3 GitHub Repository"
 git clone https://github.com/binexisHATT/3agL3.git
+Write-Output "Changing Current Directory to .\3agL3"
+Set-Location .\3agL3
+Write-Output "Running '3agL3 main.py -h' for help menu"
+python.exe .\main.py
+
--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-install() {
-    cd /opt/
-    sudo git clone https://github.com/binexisHATT/3agL3.git
-    sudo apt install python3-pip
-    sudo pip3 install -r /opt/3agL3/requirements.txt
+THIS_FILE=$0
+if [ "$EUID" != "0" ]
+then
+	echo "Please run \`${THIS_FILE:2}\` as root (sudo)"
+	exit 1
+fi
 
-   	ln -s /opt/3agL3/main.py /usr/local/bin/3agL3
-   	if [[ $? == 0 ]]
+function install_3agl3 {
+	apt install python3-pip
+	cd /opt
+	git clone https://github.com/binexisHATT/3agL3.git
+	chmod +x /opt/3agL3/main.py
+	pip3 install -r /opt/3agL3/requirements.txt
+	ln -s /opt/3agL3/main.py /bin/3agL3
+
+	if [ "$?" -eq "0" ]
    	then
-   		echo "Succesfully created symbolic link to -> /opt/3agL3/main.py"
+   		echo "-----------------------------------------------------------------------"
+   		echo "[+] Succesfully created symbolic link to -> /opt/3agL3/main.py as 3agL3"
    	else
-   		echo "Couldn't created symbolic link to -> /opt/3agL3/main.py"
+   		echo "[-] Couldn't created symbolic link to -> /opt/3agL3/main.py"
+   	fi
 }
 
-install
+install_3agl3
+
+echo "[+] Type 'sudo 3agL3 -h' for 3agL3 help menu"
