f426844a40ce3a9efc871eb89223ecc9e4653df1
sixelA zeugirdoR <44281620+binexisHATT@users.noreply.github.com>
2020-08-09 20:36:40 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,8 @@
 Write-Output "Cloning 3agL3 GitHub Repository"
 git clone https://github.com/binexisHATT/3agL3.git
+write-Output "-------------------------------------"
 Write-Output "Changing Current Directory to .\3agL3"
 Set-Location .\3agL3
-Write-Output "Running '3agL3 main.py -h' for help menu"
+Write-Output "Type '3agL3 main.py -h' for help menu"
 python.exe .\main.py
 
