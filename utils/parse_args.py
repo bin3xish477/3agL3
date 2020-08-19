00be3cac40b3eeb893a2e42e84dc3d7d0961b386
sixelA zeugirdoR <44281620+binexisHATT@users.noreply.github.com>
2020-08-19 12:43:27 -0400

    Update parse_args.py

--- utils/parse_args.py
+++ utils/parse_args.py
@@ -47,7 +47,7 @@ def parse_args():
 	read_pcap.add_argument("-ip-cnt", "--ip-count",metavar="<IP>",nargs="+",type=str,help="Prints the number of times an IP address was the source or destination IP. Multiple IP addresses can be specified")
 	read_pcap.add_argument("-ts", "--time-start",metavar="<HOUR:MINUTE>",help="Filter for packets that start from the specified hour:minute value and onwards")
 	read_pcap.add_argument("-te", "--time-end",metavar="<HOUR:MINUTE>",help="Filter for packets whose hour:minute value does not go beyond the specified hour:minute value provided")
-	read_pcap.add_argument("-tr", "--time-range",nargs="+",metavar="<HOUR:MINUTE>",help="Filter for packets whose hour:minute value falls inbetween the desired time range")
+	read_pcap.add_argument("-tr", "--time-range",nargs="+",metavar="<HOUR:MINUTE>",help="Filter for packets whose hour:minute value falls in between the desired time range")
 
 	# -------------- Writing PCAP options ---------------
 	write_pcap.add_argument("-write", "--write-mode",action="store_true", default=None,help="capture live traffic and write to PCAP file (must specify `-c` option)")
